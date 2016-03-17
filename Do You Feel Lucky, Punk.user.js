19bf8d8d845d4f1d9123db9da4c0cc4893e437e7
Nick Moore <shiny.nickel@gmail.com>
2016-03-17 16:39:38 +0000

    Attempting to fix miscounts by removing race condition

--- Do You Feel Lucky, Punk.user.js
+++ Do You Feel Lucky, Punk.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Do You Feel Lucky, Punk?
 // @namespace    http://www.steamgifts.com/user/kelnage
-// @version      0.2
-// @description  calculate the expected number of GAs you should have won based upon the GAs you've entered and the number of users who entered them
+// @version      0.3
+// @description  Calculate the expected number of GAs you should have won based upon the GAs you've entered and the number of users who entered them
 // @author       kelnage
 // @match        http://www.steamgifts.com/giveaways/entered*
 // @grant        none
@@ -19,7 +19,7 @@ var working = false;
 
 var calculateExpectedPageValue = function(input) {
     return $(".table__row-inner-wrap", input)
-        .filter(function(i) { 
+        .filter(function(i) {
             return $($(this).children().get(4)).text() == "-";  // ignore GAs that haven't finished
         })
         .map(function(i, e) {
@@ -27,6 +27,7 @@ var calculateExpectedPageValue = function(input) {
             var copies = $e.find("a.table__column__heading").text().match(/\(([0-9]+) Copies\)/); 
             copies = (copies == null ? 1 : copies[1]); // only multi-GAs have the (X Copies) text in their title, default to 1 copy
             var entries = $($e.children().get(2)).text().replace(/,/, ""); // remove number formatting
+            // console.debug(copies, entries, copies / entries)
             return copies / entries;
         })
         .get() // turn it into an array
@@ -42,25 +43,26 @@ var calculateTotalExpectedValue = function(evt) {
     if(!working) {
         working = true;
         $("span#punk_result").text("Calculating your odds of success now. Please be patient - this should take about 1 minute");
-        var totalExpectedValue = 0;
+        var totalExpectedValue = 0, finished = 0;
         for(var i = 1; i <= lastPage; i++) {
             setTimeout((function(i) { // using a closure because javascript
                 return function() {
                     fetchPage(i, function(data) {
                         var exp = calculateExpectedPageValue(data);
                         totalExpectedValue += exp;
-                        if(i == lastPage) {
+                        finished += 1;
+                        if(finished == lastPage) {
                             $("span#punk_result").html("Based on the finished GAs you have entered, you would expect to have won about <strong>" + 
                                                        new Number(totalExpectedValue).toFixed(1) + "</strong> of them");
                             working = false;
                         } else {
                             $("span#punk_result").text("Calculating your odds of success now. Please be patient - this should take another " + 
-                                                       new Number(MAX_DURATION - ((i - 1) * (MAX_DURATION / lastPage))).toFixed(0) + " seconds");
+                                                       new Number(MAX_DURATION - ((finished - 1) * (MAX_DURATION / lastPage))).toFixed(0) + " seconds");
                         }
                     });
                 }
             })(i), (i - 1) * ((MAX_DURATION * 1000) / lastPage));
-            // take about 1 minute to fetch all pages, but always take at least 300ms per req to try to avoid overloading SG
+            // take about 1 minute to fetch all pages, won't look at more than 200 pages
         }
     }
     return false;
